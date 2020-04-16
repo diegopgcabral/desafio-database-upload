6beedddfa9a98bd332a4457e20be8043064c8031
Diego Cabral <diegopgc@gmail.com>
2020-04-15 22:26:16 -0300

    first commit

---
+++ src/database/migrations/1586987450929-CreateTransactions.ts
@@ -0,0 +1,8 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTransactions1586987450929
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
